ee71e8a5f78cbce8a5332be3b387c7df841d8268
hamaji <k.hamaji@gmail.com>
2015-02-05 19:30:02 +0900

    新規

---
+++ SampleAddin/XmlEditor.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SampleAddin
+{
+    partial class XmlEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.xmlDataGridView = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xmlDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(23, 13);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(50, 12);
+            this.fileNameLabel.TabIndex = 0;
+            this.fileNameLabel.Text = "fileName";
+            // 
+            // xmlDataGridView
+            // 
+            this.xmlDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.xmlDataGridView.Location = new System.Drawing.Point(25, 68);
+            this.xmlDataGridView.Name = "xmlDataGridView";
+            this.xmlDataGridView.RowTemplate.Height = 21;
+            this.xmlDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.xmlDataGridView.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(104, 226);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "button1";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // XmlEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.xmlDataGridView);
+            this.Controls.Add(this.fileNameLabel);
+            this.Name = "XmlEditor";
+            this.Text = "XmlEditor";
+            this.Load += new System.EventHandler(this.XmlEditor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.xmlDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.DataGridView xmlDataGridView;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ SampleAddin/XmlEditor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE80;
+using EnvDTE;
+
+namespace SampleAddin
+{
+    public partial class XmlEditor : Form
+    {
+        DTE2 _applicationObject;
+        private DataSet _dataSet;
+        private string _xmlFilePath;
+
+        public XmlEditor(DTE2 applicationObject)
+        {
+            // 呼び出し元のConnectクラスから
+            // アプリケーション・オブジェクトを引数で受け取り、
+            // それをフィールド変数にセットします
+            _applicationObject = applicationObject;
+
+            InitializeComponent();
+        }
+
+        private void XmlEditor_Load(object sender, EventArgs e)
+        {
+            // ［ソリューション エクスプローラ］で現在選択されている
+            // ファイルのコレクションを取得します
+            SelectedItems items = _applicationObject.SelectedItems;
+
+            // ファイルのコレクションの中から1つ目のアイテムを取得します
+            SelectedItem item = items.Item(1);
+
+            // ファイル・パスを取得してフィールド変数にセットします
+            _xmlFilePath = item.ProjectItem.get_FileNames(1);
+            _dataSet = new DataSet();
+            try
+            {
+                // データセットにファイルを読み込み、
+                // 内容をデータグリッドビューに表示します
+                _dataSet.ReadXml(_xmlFilePath);
+                xmlDataGridView.DataSource = _dataSet.Tables[0];
+
+                // 読み込んだファイル名をラベルに表示します
+                fileNameLabel.Text = item.Name;
+            }
+            catch (Exception)
+            {
+                // 読み込んだファイルがXML形式でない場合、
+                // エラー・メッセージをラベルに表示して、
+                // ［保存］ボタンを無効化します
+                fileNameLabel.Text = "ファイルの読み込みに失敗しました";
+                saveButton.Enabled = false;
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // データグリッドビューで編集された内容を
+            // XMLファイルに保存します
+            _dataSet.WriteXml(_xmlFilePath);
+        }
+    }
+}
